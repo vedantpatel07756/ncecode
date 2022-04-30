777b911e3fa9461cd236a9517141855de43e8cca
vedantpatel07756 <vedantpatel07756@gmail.com>
2022-04-30 22:45:13 +0530

    changes

---
+++ build/precache-manifest.0c94684b6bf4af7dc0ef8413f3e75c10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b9c992a48e0b9e3f857",
+    "url": "/static/css/main.005a2a28.chunk.css"
+  },
+  {
+    "revision": "9b9c992a48e0b9e3f857",
+    "url": "/static/js/main.9b9c992a.chunk.js"
+  },
+  {
+    "revision": "709d58a229027795f766",
+    "url": "/static/js/1.709d58a2.chunk.js"
+  },
+  {
+    "revision": "6a082f033d73e3803c25",
+    "url": "/static/js/2.6a082f03.chunk.js"
+  },
+  {
+    "revision": "6ee34327ee87b8f52b89",
+    "url": "/static/js/runtime~main.6ee34327.js"
+  },
+  {
+    "revision": "95106087a1b9590e90bee9189cc7467e",
+    "url": "/index.html"
+  }
+];
